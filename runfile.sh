e1058cec6a64d5b6938352beae78c034428def08
dhananjayharel <dhananjayharel@users.noreply.github.com>
2019-09-19 15:07:28 +0530

    api toke changed to info acc

--- runfile.sh
+++ runfile.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 echo "Deploying "$1
-curl -s https://tok_4fd1je58x56gy2ybnxddzhhxb0@api.appetize.io/v1/apps -F "file=@$1/app/build/outputs/apk/debug/app-debug.apk" -F "platform=android" > .outputurl
+curl -s https://tok_fpym7vqveexadk8t3m9ru9cg7m@api.appetize.io/v1/apps -F "file=@$1/app/build/outputs/apk/debug/app-debug.apk" -F "platform=android" > .outputurl
 echo "done"
